faa1307be6d390f3e041972d2c9c37e769876b13
Elijha Couch <31853975+ElijhaC@users.noreply.github.com>
2023-06-09 00:08:51 -0400

    Loading img added

--- script.js
+++ script.js
@@ -3,20 +3,48 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote'); 
+const loader = document.getElementById('loader'); 
 
 let apiQuotes = [];
 
+//Show loading
+function loading(){
+    
+    quoteContainer.hidden = true;
+    
+}
+
+//Hide loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 //Show new Quote
 function newQuote(){
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    authorText.textContent = quote.author;
-     quoteText.textContent = quote.text;
-    console.log(quote);
+     // check if author feild is blank
+     if(!quote.author){
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check quote length to determine the styling
+    if(quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    //Set Quote, Hide Loader
+   quoteText.textContent = quote.text;
+   complete();
 }
 
 // GEt Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
